07a552086cd91d30aea5662feb44bf14491cc852
Beverly Mugwadi <b.mugwadi1@alustudent.com>
2025-09-12 11:08:36 +0200

    Correcton

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -1,33 +1,60 @@
 #!/usr/bin/python3
-""" BaseCaching module
-"""
+""" LFU Caching Module """
+from base_caching import BaseCaching
 
-class BaseCaching():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
+
+class LFUCache(BaseCaching):
+    """ LFU caching system """
 
     def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
+        """Initialize the cache"""
+        super().__init__()
+        self.freq = {}       # Track frequency of use
+        self.order = {}      # Track order for LRU tie-breaking
+        self.counter = 0     # Global counter for order
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError("put must be implemented in your cache class")
+        """Add an item to the cache"""
+        if key is None or item is None:
+            return
+
+        # Insert or update item
+        self.cache_data[key] = item
+        # Update frequency
+        self.freq[key] = self.freq.get(key, 0) + 1
+        # Update order (for LRU in case of tie)
+        self.counter += 1
+        self.order[key] = self.counter
+
+        # If cache exceeds max size -> remove LFU (or LRU of LFU)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # Find the minimum frequency
+            min_freq = min(self.freq.values())
+            # Candidates = all keys with min frequency
+            candidates = [
+                k for k, v in self.freq.items() if v == min_freq
+            ]
+            if len(candidates) == 1:
+                discard_key = candidates[0]
+            else:
+                # Apply LRU among candidates
+                discard_key = min(candidates, key=lambda k: self.order[k])
+
+            # Remove from all tracking dicts
+            del self.cache_data[discard_key]
+            del self.freq[discard_key]
+            del self.order[discard_key]
+
+            print("DISCARD: {}".format(discard_key))
 
     def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError("get must be implemented in your cache class")
-    
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        # Increase frequency and update order
+        self.freq[key] += 1
+        self.counter += 1
+        self.order[key] = self.counter
+
+        return self.cache_data[key]
